35b72304126a1ff23a743fa3d40c2af98d42731b
konomi-kyoso-programming <konomi2010@gmail.com>
2025-07-16 12:08:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,25 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(worksheet="sheet1", ttl=5)
 
 st.dataframe(df)
+
+STATUS = ["IN USE", "AVAILABLE"]
+
+with st.form(key="the_form"):
+    item = st.text_input(label="Item*")
+    status = st.selectbox("Status", options=STATUS)
+    entry_date = st.date_input(label="Entry date")
+    
+    submit_button = st.form_submit_button(label="Submit")
+
+if submit_button:
+    #st.write("Submission done!")
+
+    if not item:
+        st.warning("you must fill the item field.")
+        st.stop()
+    else:
+        the_data = pd.DataFrame([
+            {"item": item, "status": status, "entry_date": entry_date}
+        ])
+    updated_df = pd.concat([df, the_data], ignore_index=True)
+    conn.update(worksheet="sheet1", data=updated_df)
